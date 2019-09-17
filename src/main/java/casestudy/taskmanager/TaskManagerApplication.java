5d71e06786f24a48688b493758ce2cd877f14bb6
avik-fse <avikonline@gmail.com>
2019-09-16 23:43:51 -0400

    Changes for building the application as war

--- src/main/java/casestudy/taskmanager/TaskManagerApplication.java
+++ src/main/java/casestudy/taskmanager/TaskManagerApplication.java
@@ -2,9 +2,15 @@ package casestudy.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TaskManagerApplication {
+public class TaskManagerApplication extends SpringBootServletInitializer {
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(TaskManagerApplication.class);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(TaskManagerApplication.class, args);
